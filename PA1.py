fc49cc097ac25a35bee7a897a7e7693a472fbd9b
LeCroonch <alejandro.fernandez2001@yahoo.com>
2024-06-17 18:42:38 -0400

    Update PA1.py

--- PA1.py
+++ PA1.py
@@ -53,7 +53,7 @@ def fifo(runfor, processcount, processes):
         output_lines.append(f"Process {process_id} wait {wait:3} turnaround {turnaround:3} response {response:3}")
 
     # Write the output to a file
-    with open('output.txt', 'w') as file:
+    with open('output.out', 'w') as file:
         for line in output_lines:
             file.write(line + '\n')
 
@@ -64,9 +64,8 @@ def rr(runfor, processcount, processes, quantum):
     output_lines = []
     current_time = 0
     queue = []
-    finished_processes = []
     in_queue = set()
-    process_stats = {process_id: {'wait': 0, 'turnaround': 0, 'response': -1, 'remaining_burst': burst, 'last_seen': None} for arrival, burst, process_id in processes}
+    process_stats = {process_id: {'wait': 0, 'turnaround': 0, 'response': -1, 'remaining_burst': burst} for arrival, burst, process_id in processes}
 
     processes = sorted(processes, key=lambda x: x[0])  # Sort processes by arrival time
 
@@ -75,7 +74,11 @@ def rr(runfor, processcount, processes, quantum):
     output_lines.append(f"Quantum   {quantum}")
     output_lines.append("")
 
+    wait_times = {process_id: 0 for _, _, process_id in processes}
+    last_seen = {process_id: -1 for _, _, process_id in processes}
+
     while current_time < runfor:
+        # Add newly arrived processes to the queue
         for arrival, burst, process_id in processes:
             if arrival == current_time and process_id not in in_queue:
                 queue.append(process_id)
@@ -85,34 +88,40 @@ def rr(runfor, processcount, processes, quantum):
         if queue:
             process_id = queue.pop(0)
             in_queue.remove(process_id)
-            arrival = [arr for arr, brst, pid in processes if pid == process_id][0]
-            burst = process_stats[process_id]['remaining_burst']
+            arrival, burst = [(arr, brst) for arr, brst, pid in processes if pid == process_id][0]
+            remaining_burst = process_stats[process_id]['remaining_burst']
 
             if process_stats[process_id]['response'] == -1:
                 process_stats[process_id]['response'] = current_time - arrival
-            run_time = min(quantum, burst)
-            output_lines.append(f"Time {current_time:3} : {process_id} selected (burst {burst:3})")
 
-            for t in range(run_time):
+            if last_seen[process_id] != -1:
+                wait_times[process_id] += (current_time - last_seen[process_id])
+
+            run_time = min(quantum, remaining_burst)
+            output_lines.append(f"Time {current_time:3} : {process_id} selected (burst {remaining_burst:3})")
+
+            for _ in range(run_time):
                 current_time += 1
-                burst -= 1
-                process_stats[process_id]['remaining_burst'] = burst
-                for arr, brst, proc_id in processes:
-                    if arr == current_time and proc_id not in in_queue:
-                        queue.append(proc_id)
-                        in_queue.add(proc_id)
-                        output_lines.append(f"Time {current_time:3} : {proc_id} arrived")
+                remaining_burst -= 1
+                # Add newly arrived processes during the execution
+                for arr, brst, pid in processes:
+                    if arr == current_time and pid not in in_queue:
+                        queue.append(pid)
+                        in_queue.add(pid)
+                        output_lines.append(f"Time {current_time:3} : {pid} arrived")
                 if current_time >= runfor:
                     break
-            if burst > 0:
+
+            process_stats[process_id]['remaining_burst'] = remaining_burst
+
+            if remaining_burst > 0:
                 queue.append(process_id)
                 in_queue.add(process_id)
-                process_stats[process_id]['last_seen'] = current_time
+                last_seen[process_id] = current_time
             else:
-                finished_processes.append(process_id)
                 process_stats[process_id]['turnaround'] = current_time - arrival
-                process_stats[process_id]['wait'] = process_stats[process_id]['turnaround'] - (process_stats[process_id]['remaining_burst'] + run_time)
                 output_lines.append(f"Time {current_time:3} : {process_id} finished")
+                last_seen[process_id] = -1
         else:
             output_lines.append(f"Time {current_time:3} : Idle")
             current_time += 1
@@ -123,11 +132,11 @@ def rr(runfor, processcount, processes, quantum):
     for process_id in sorted(process_stats.keys()):
         turnaround = process_stats[process_id]['turnaround']
         response = process_stats[process_id]['response']
-        wait = turnaround - (process_stats[process_id]['remaining_burst'] if process_id in finished_processes else process_stats[process_id]['remaining_burst'] + quantum)
+        wait = wait_times[process_id]
         output_lines.append(f"{process_id} wait {wait:3} turnaround {turnaround:3} response {response:3}")
 
     # Write the output to a file
-    with open('output.txt', 'w') as file:
+    with open('output.out', 'w') as file:
         for line in output_lines:
             file.write(line + '\n')
 
